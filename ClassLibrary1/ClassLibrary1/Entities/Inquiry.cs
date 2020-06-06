8c64013d7e4b80f3897a4e58d2ba8724670ea95b
Misha Zaharkin <zaharovm70@gmail.com>
2020-06-07 01:50:00 +0300

    Added DAL Entities

---
+++ ClassLibrary1/ClassLibrary1/Entities/Inquiry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Entities
+{
+    class Inquiry
+    {
+        public int InquiryID { get; set; }
+        public char Date { get; set; }
+        public List<TemparatureData> Data { get; set; }
+        public List<Localitycs> Place { get; set; }
+    }
+}
