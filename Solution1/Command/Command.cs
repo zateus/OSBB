516a9ce34a03c2d6178ca4068d57f19fd6913467
Misha Zaharkin <zaharovm70@gmail.com>
2020-06-11 20:46:57 +0300

    Added 12,13 lab

---
+++ Solution1/Command/Command.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Command
+{
+    abstract class Command
+    {
+        public abstract void Execute();
+        public abstract void Undo();
+    }
+   
+    class ConcreteCommand : Command
+    {
+        Receiver receiver;
+        public ConcreteCommand(Receiver r)
+        {
+            receiver = r;
+        }
+        public override void Execute()
+        {
+            receiver.Operation();
+        }
+
+        public override void Undo()
+        { }
+    }
+
+    
+    class Receiver
+    {
+        public void Operation()
+        { }
+    }
+    
+    class Invoker
+    {
+        Command command;
+        public void SetCommand(Command c)
+        {
+            command = c;
+        }
+        public void Run()
+        {
+            command.Execute();
+        }
+        public void Cancel()
+        {
+            command.Undo();
+        }
+    }
+    class Client
+    {
+        void Main()
+        {
+            Invoker invoker = new Invoker();
+            Receiver receiver = new Receiver();
+            ConcreteCommand command = new ConcreteCommand(receiver);
+            invoker.SetCommand(command);
+            invoker.Run();
+        }
+    }
+}
